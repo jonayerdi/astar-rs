c38ac4f8cdf535f92f5d89523939771a365dec8b
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2021-06-20 19:50:10 +0200

    refactor Path as linked list

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@ use std::{
     cmp::Ordering,
     collections::{BinaryHeap, HashSet},
     hash::Hash,
-    iter,
+    mem::{self, MaybeUninit},
     ops::Add,
+    rc::Rc,
 };
 
 pub trait Node: Copy + Eq + Hash {
@@ -14,34 +15,47 @@ pub trait Node: Copy + Eq + Hash {
     fn minimum_remaining_cost(&self, goal: &Self) -> <Self as Node>::Cost;
 }
 
+struct PathNode<N: Node> {
+    pub node: N,
+    pub prev: Option<Rc<PathNode<N>>>,
+}
+
 struct Path<N: Node> {
-    nodes: Vec<N>,
+    last_node: Rc<PathNode<N>>,
     cost: N::Cost,
     goal: N,
+    length: usize,
 }
 
 impl<N: Node> Path<N> {
     fn new(start: N, goal: N) -> Self {
         Self {
-            nodes: vec![start],
+            last_node: Rc::new(PathNode {
+                node: start,
+                prev: None,
+            }),
             cost: Default::default(),
             goal,
+            length: 1,
         }
     }
     fn last(&self) -> N {
-        self.nodes[self.nodes.len() - 1]
+        self.last_node.node
     }
     fn minimum_total_cost(&self) -> N::Cost {
         self.cost + self.last().minimum_remaining_cost(&self.goal)
     }
     fn next_move(&self, node: N) -> Self {
-        let mut nodes = Vec::with_capacity(self.nodes.len() + 1);
-        nodes.extend(self.nodes.iter().map(|n| *n).chain(iter::once(node)));
+        let last_node = Rc::new(PathNode {
+            node,
+            prev: Some(Rc::clone(&self.last_node)),
+        });
         let cost = self.cost + self.last().move_cost(&node);
         Self {
-            nodes,
+            last_node,
             cost,
             goal: self.goal,
+            length: self.length + 1,
         }
     }
 }
@@ -63,7 +77,10 @@ impl<N: Node> PartialOrd for Path<N> {
 impl<N: Node> Ord for Path<N> {
     fn cmp(&self, other: &Self) -> Ordering {
         // We panic if partial_cmp fails.
-        other.minimum_total_cost().partial_cmp(&self.minimum_total_cost()).unwrap()
+        other
+            .minimum_total_cost()
+            .partial_cmp(&self.minimum_total_cost())
+            .unwrap()
     }
 }
 
@@ -76,7 +93,7 @@ fn solve<N: Node>(start: N, goal: N) -> Option<(Vec<N>, N::Cost)> {
     paths.push(Path::new(start, goal));
 
     while let Some(path) = paths.pop() {
-        let current = path.nodes[path.nodes.len() - 1];
+        let current = path.last();
         let _ = visited.insert(current);
         if current == goal {
             solution = Some(path);
@@ -89,7 +106,24 @@ fn solve<N: Node>(start: N, goal: N) -> Option<(Vec<N>, N::Cost)> {
         }
     }
 
-    solution.map(|p| (p.nodes, p.cost))
+    solution.map(|p| {
+        let mut current = &p.last_node;
+        let mut path: Vec<MaybeUninit<N>> = (0..p.length).map(|_| MaybeUninit::uninit()).collect();
+        for node in path.iter_mut().rev() {
+            *node = MaybeUninit::new(current.node);
+            match &current.prev {
+                Some(c) => current = c,
+                None => {}
+            };
+        }
+        (
+            unsafe {
+                // SAFETY: All the elements in `path` have been initialized.
+                mem::transmute(path)
+            },
+            p.cost,
+        )
+    })
 }
 
 #[cfg(test)]
@@ -133,8 +167,7 @@ mod tests {
             AdjacentPositionsIter::new(*self)
         }
         fn move_cost(&self, next: &Self) -> Self::Cost {
-            (((self.0 - next.0).pow(2) + (self.1 - next.1).pow(2)) as f64)
-                .sqrt()
+            (((self.0 - next.0).pow(2) + (self.1 - next.1).pow(2)) as f64).sqrt()
         }
         fn minimum_remaining_cost(&self, goal: &Self) -> Self::Cost {
             self.move_cost(goal)
